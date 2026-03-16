2e896e4a2ff88184fbab38d5d9cdfd6062021914
abatovaziz227-sudo <abatovaziz227@gmail.com>
2026-03-17 00:34:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,3 @@ toggle.addEventListener('click', () => {
     }
 });
 
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-const toggle = document.querySelector(".theme-toggle");
-
-toggle.addEventListener("click", () => {
-document.body.classList.toggle("light");
-});
